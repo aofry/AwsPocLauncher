9d0cc0aba3f0acb86714b36e13d0aa0e755ec9da
Adi Ofry <aofry@yahoo.com>
2015-07-21 14:56:41 +0300

    added calabash simple test for clicking the button and finding a text result

---
+++ calabash/features/step_definitions/calabash_steps.rb
@@ -0,0 +1,11 @@
+require 'calabash-android/calabash_steps'
+
+#Given(/^I touch the "(.*?)" button$/) do |arg1|
+#  pending # express the regexp above with the code you wish you had
+#end
+
+#When /^When I enter valid username and password$/ do
+#enter_text("webView css:'#Email'", "abc@gmail.com")
+#enter_text("webView css:'#Passwd'","test123")
+#end
+
