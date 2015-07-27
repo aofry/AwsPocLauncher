073f4c3fa8ece04da9386d3874ef091ad01592c4
aofry <aofry@yahoo.com>
2015-07-27 14:01:11 +0300

    added UIAutomator test

---
+++ app/src/androidTest/java/com/hp/awspoclauncher/UiAutomTest.java
@@ -0,0 +1,42 @@
+package com.hp.awspoclauncher;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
+import android.support.test.uiautomator.Until;
+import android.test.InstrumentationTestCase;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.By;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UiAutomTest extends InstrumentationTestCase {
+
+    private UiDevice mDevice;
+
+    @Before
+    public void setUp() {
+        // Initialize UiDevice instance
+        mDevice = UiDevice.getInstance(getInstrumentation());
+
+        // Start from the home screen
+        //mDevice.pressHome();
+        //mDevice.wait(Until.hasObject(By.pkg(getHomeScreenPackage()).depth(0)),
+        Context context = getInstrumentation().getContext();
+        Intent intent = context.getPackageManager()
+                .getLaunchIntentForPackage("com.hp.awspoclauncher");
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        // Clear out any previous instances
+        context.startActivity(intent);
+        mDevice.wait(Until.hasObject(By.pkg("com.hp.awspoclauncher").depth(0)), 10000);
+    }
+
+    @Test
+    public void testTwoPlusThreeEqualsFive() throws UiObjectNotFoundException {
+        mDevice.findObject(new UiSelector()
+                .packageName("com.hp.awspoclauncher").resourceId(String.valueOf(R.id.getDataBtn))).click();
+
+    }
+}
